c4f1f2760f9b37e1de884489deea3b00ec1bbbe1
Filiph Siitam Sandström <filiph@nordcom.io>
2023-08-26 05:55:55 +0200

    Editor: Fix GraphiQL IDE hanging.
    This was due to us trying to register the custom fields way too early.
    The proper solutions is to wait for `graphql_init`.

--- wp-graphql-bebuilder.php
+++ wp-graphql-bebuilder.php
@@ -15,8 +15,9 @@
  * Version:                     0.1.0
  * Author:                      Nordcom Group Inc.
  * Author URI:                  https://nordcom.io/
+ * Tags:                        Headless WordPress, BeBuilder, WPGraphQL, GraphQL, Betheme
  * Text Domain:                 wp-graphql-bebuilder
- * Domain Path:                 /languages/
+ * Domain Path:                 /languages
  * Tested up to:                6.3
  * Requires PHP:                8.0
  * Requires WP:                 6.0
@@ -26,30 +27,61 @@
  * License URI:                 https://github.com/NordcomInc/wp-graphql-bebuilder/blob/main/LICENSE
  */
 
-namespace WPGraphQL\Extensions;
-
 // Deny direct access.
-if (!defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
     exit;
 }
 
-add_action('graphql_register_types', function() {
-    register_graphql_field( 'Page', 'mfnItems', [
-        'type' => 'String',
-        'description' => __( 'Page items', 'wp-graphql' ),
-        'resolve' => function( $post ) {
-            $items = get_post_meta( $post->ID, 'mfn-page-items', true );
-            return ! empty( $items ) ? json_encode(unserialize(base64_decode($items))) : '';
-        }
-    ] );
+add_action('admin_init', function () {
+    $core_dependencies = [
+        'WPGraphQL' => class_exists('WPGraphQL'),
+        'BeBuilder' => function_exists('mfn_global'),
+    ];
+
+    $missing_dependencies = array_keys(array_diff($core_dependencies, array_filter($core_dependencies)));
+    $display_admin_notice = static function() use ($missing_dependencies) {
+        ?>
+            <div class="notice notice-error">
+                <p>
+                    <?php esc_html_e(
+                        'The WP GraphQL BeBuilder plugin can\'t be properly activated because the following dependencies are unavailable or not installed:',
+                        'wp-graphql-bebuilder'
+                    ); ?>
+                </p>
+                <ul>
+                    <?php foreach ($missing_dependencies as $missing_dependency): ?>
+                        <li><?php echo esc_html($missing_dependency); ?></li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        <?php
+    };
+
+    if (!empty($missing_dependencies)) {
+        add_action('network_admin_notices', $display_admin_notice);
+        add_action('admin_notices', $display_admin_notice);
+
+        return;
+    }
 });
-add_action('graphql_register_types', function() {
-    register_graphql_field( 'Page', 'mfnItemsSeo', [
-        'type' => 'String',
-        'description' => __( 'Page items\' SEO', 'wp-graphql' ),
-        'resolve' => function( $post ) {
-            $items = get_post_meta( $post->ID, 'mfn-page-items-seo', true );
-            return ! empty( $items ) ? $items : '';
-        }
-    ] );
+add_action('graphql_init', function() {
+    add_action('graphql_register_types', function() {
+        register_graphql_field('Page', 'mfnItems', [
+            'type' => 'String',
+            'description' => __('Page Components', 'wp-graphql-bebuilder'),
+            'resolve' => function($post) {
+                $items = get_post_meta($post->ID, 'mfn-page-items', true);
+                return ! empty($items) ? json_encode(unserialize(base64_decode($items))) : '';
+            }
+        ]);
+    
+        register_graphql_field('Page', 'mfnItemsSeo', [
+            'type' => 'String',
+            'description' => __('Page SEO Components', 'wp-graphql-bebuilder'),
+            'resolve' => function($post) {
+                $items = get_post_meta($post->ID, 'mfn-page-items-seo', true);
+                return ! empty($items) ? $items : '';
+            }
+        ]);
+    });    
 });
